d18acb8ac6231bb7be655e6a1a6f65f401ad1e4d
durgaprasadmaddala <durgaprasadmaddalaa@gmail.com>
2026-08-20 08:49:56 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,28 @@
+package Datatyperanges;
+
+public class Data {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		// byte range
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+
+
+
+	}
+
+}
