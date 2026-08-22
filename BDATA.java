4801c5f54cbe378ed02af88469e1a6de3f9e19ab
MADDALA DURGAPRASAD <durgaprasadmaddalaa@gmail.com>
2026-08-22 22:32:54 +0530

    Add files via upload

---
+++ BDATA.java
@@ -0,0 +1,25 @@
+package Datatyperanges;
+
+public class BDATA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// byte range
+		 System.out.println(Byte.MIN_VALUE);
+		 
+		 System.out.println(Byte.MAX_VALUE);
+		 
+System.out.println(Short.MIN_VALUE);
+		 
+		 System.out.println(Short.MAX_VALUE);
+System.out.println(Integer.MIN_VALUE);
+		 
+		 System.out.println(Byte.MAX_VALUE);
+System.out.println(Byte.MIN_VALUE);
+		 
+		 System.out.println(Byte.MAX_VALUE);
+
+
+	}
+
+}
